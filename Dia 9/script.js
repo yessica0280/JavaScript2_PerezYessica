6948a593c69bd40b53d32b52e74d18309380c583
yessica perez <yp80905@gmail.com>
2024-09-02 18:06:13 -0500

    feat :bug:Arrangements

--- Dia 9/script.js
+++ Dia 9/script.js
@@ -1,4 +1,7 @@
 document.getElementById('letra').addEventListener('input',ver)
+document.getElementById('next').addEventListener('click',siguiente)
+document.getElementById('prev').addEventListener('click',anterior)
+let current=null;
 function ver(){
     var i =document.getElementById("letra").value;
     var leer='https://pokeapi.co/api/v2/pokemon/'+i
@@ -8,14 +11,36 @@ function poke(leer){
     fetch(leer)
     .then(res=> res.json())
     .then(data => {
-        pasos=data.sprites.other.showdown.front_default
-        Name=data.name
-        id=data.id
+        current=data.id
+        let pasos=data.sprites.other.showdown.front_default
+        let Name=data.name
+        let id=data.id
         document.getElementById("resultado").innerHTML=""
         document.getElementById("resultado").innerHTML+=`
-            <img src="${pasos}" style="width: ">
-            ${Name}
+            <img src="${pasos}">
+        `
+        document.getElementById("resultado1").innerHTML=""
+        document.getElementById("resultado1").innerHTML+=`
             ${id}
         `
+        document.getElementById("resultado2").innerHTML=""
+        document.getElementById("resultado2").innerHTML+=`
+            ${Name}
+        `;
+        pokemon();
     })
+}
+function siguiente(){
+    if (current !== null){
+        let nextid=current+1;
+        var leer ="https://pokeapi.co/api/v2/pokemon/"+nextid;
+        poke(leer)
+    }
+}
+function anterior(){
+    if (current !== null && current > 1){
+        let previd=current-1;
+        var leer ="https://pokeapi.co/api/v2/pokemon/"+previd;
+        poke(leer)
+    }
 }
